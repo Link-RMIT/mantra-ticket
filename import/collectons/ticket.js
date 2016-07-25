47d728775822e11bb247905240e53546767224d4
link <s3508932@student.rmit.edu.au>
2016-07-25 17:39:00 +1000

    init

---
+++ import/collectons/ticket.js
@@ -0,0 +1,26 @@
+import {Mongo} from 'meteor/mongo';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+Pins.deny({
+    insert(){ return true; },
+    update(){ return true; },
+    remove(){ return true; },
+});
+
+Pins.schema = new SimpleSchema({
+    _id: { type: String, regEx: SimpleSchema.RegEx.Id},
+    title: { type: String },
+    createdAt: { type: Date,  denyUpdate: true },
+    userId: { type: String, regEx: SimpleSchema.RegEx.Id, optional: true },
+    url: { type: String, regEx: SimpleSchema.RegEx.Url },
+});
+
+Pins.publicFields = {
+    title: 1,
+    createdAt: 1,
+    url: 1,
+    userId: 1,
+};
+
+//"meteor add aldeed:collection2" required
+Pins.attachSchema(Pins.schema);
