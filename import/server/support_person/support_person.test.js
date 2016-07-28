f185679e3f5c4501a33baf12a3a73fc5853f33ed
link <s3508932@student.rmit.edu.au>
2016-07-28 16:33:37 +1000

    some issues remain unsolved in collecting publications after updating fields, test results are unstable, looks like caused by asynchronous

--- import/server/support_person/support_person.test.js
+++ import/server/support_person/support_person.test.js
@@ -6,7 +6,8 @@ import { _ } from 'meteor/underscore';
 import { Roles } from 'meteor/alanning:roles';
 import { PublicationCollector } from 'meteor/johanbrook:publication-collector';
 
-import { Cases,State } from '../../collectons/cases.js';
+import { Cases,State } from '../../collections/cases.js';
+import { CaseNotes } from '../../collections/case_notes.js';
 import { MethodNames, Configs, Publications } from '../../definitions';
 import { assign_case_to_another_team, assign_case_to_self, release } from './methods.js';
 
@@ -22,7 +23,7 @@ if (Meteor.isServer) {
             return Accounts.createUser({
                 email: name+"@theCompany.com",
                 password: "password",
-                profile: { name: name}
+                username: name,
             });
         });
         const team = {
@@ -52,7 +53,42 @@ if (Meteor.isServer) {
                 });
                 collector.collect(Publications.cases.list.support,(collections) => {
                     assert.equal(collections.Cases.length, 1);
+                    assert.equal(collections.Cases[0]._id, caseId);
                     done();
+                });
+            });
+            describe('foo', ()=>{
+                beforeEach(() => {
+                    Cases.update(caseId,{$set:{supportPersonId:team.level1[0]}});
+                });
+                it('can view cases assigned to itself',(done)=>{
+                    const collector = new PublicationCollector({
+                        userId: team.level1[0]
+                    });
+                    //console.log('//before collect////////////////////');
+                    collector.collect(Publications.cases.list.support,(collections) => {
+                        assert.equal(collections.Cases.length, 1);
+                        assert.equal(collections.Cases[0]._id, caseId);
+                        assert.equal(collections.Cases[0].supportPersonId, team.level1[0]);
+
+                        done();
+                    });
+                    //console.log('//after collect////////////////////');
+                });
+                it('can\'t view cases assigned to others',(done)=>{
+                    //console.log(Cases.findOne({_id:caseId}));
+                    //console.log(Cases.find({}).count());
+                    const collector = new PublicationCollector({
+                        userId: team.level1[1]
+                    });
+                    collector.collect(Publications.cases.list.support,(collections) => {
+                        if(collections.Cases){
+                            //const foo = Cases.findOne({_id:caseId});
+                            //console.log(foo,collections.Cases[0]);
+                            assert.equal(collections.Cases.length, 0);
+                        }
+                        done();
+                    });
                 });
             });
             it('can\'t view cases in other team\'s queue',(done)=>{
@@ -66,144 +102,120 @@ if (Meteor.isServer) {
                     done();
                 });
             });
-        });
+            describe('comments',()=>{
+
+            });//end comments
+        });// end publications
         describe('methods',()=>{
-            const assign_case_to_another_team = Meteor.server.method_handlers[
-                MethodNames.case.assign.to.team
-            ];
-            const assign_case_to_self = Meteor.server.method_handlers[
-                MethodNames.case.assign.to.self
-            ];
-            const release = Meteor.server.method_handlers[
-                MethodNames.case.release
-            ];
-            it('can assign an case to another team',(done)=>{
-                assign_case_to_another_team.apply({
-                    userId: team.level1[0],
-                    user: Meteor.users.findOne({_id:team.level1[0] }),
-                },[{caseId,teamName:'Developer'}]);
-                const the_case = Cases.findOne({
-                    _id:caseId
-                });
-                assert(the_case.supportTeamName, 'Developer');
-                assert.isUndefined(the_case.supportPersonId);
-                assert(the_case.state, State.PENDING);
-                done();
-            });
-
-            it('can\'t assign an case belong to other teams',()=>{
-                expect(()=>{
+            describe('assigning cases',()=>{
+                const assign_case_to_another_team = Meteor.server.method_handlers[
+                    MethodNames.case.assign.to.team
+                ];
+                const assign_case_to_self = Meteor.server.method_handlers[
+                    MethodNames.case.assign.to.self
+                ];
+                const release = Meteor.server.method_handlers[
+                    MethodNames.case.release
+                ];
+                it('can assign an case to another team',(done)=>{
                     assign_case_to_another_team.apply({
-                        userId: team.developer[0],
-                        user: Meteor.users.findOne({_id:team.developer[0] }),
-                    },[{caseId,teamName:Config.DEFAULT_SUPPORT_TEAM}]);
-                }).to.throw();
-            });
-
-            it('can\'t assign an case assigned to another support person',()=>{
-                Cases.update(caseId,{$set:{supportPersonId:team.level1[0]}});
-                expect(()=>{
-                    assign_case_to_another_team.apply({
-                        userId: team.level1[1],
-                        user: Meteor.users.findOne({_id:team.level1[1] }),
+                        userId: team.level1[0],
+                        user: Meteor.users.findOne({_id:team.level1[0] }),
                     },[{caseId,teamName:'Developer'}]);
-                }).to.throw();
+                    const the_case = Cases.findOne({
+                        _id:caseId
+                    });
+                    assert.equal(the_case.supportTeamName, 'Developer');
+                    assert.isUndefined(the_case.supportPersonId);
+                    assert.equal(the_case.state, State.PENDING);
+                    done();
+                });
+
+                it('can\'t assign an case belong to other teams',()=>{
+                    expect(()=>{
+                        assign_case_to_another_team.apply({
+                            userId: team.developer[0],
+                            user: Meteor.users.findOne({_id:team.developer[0] }),
+                        },[{caseId,teamName:Config.DEFAULT_SUPPORT_TEAM}]);
+                    }).to.throw();
+                });
+
+                it('can\'t assign an case assigned to another support person',()=>{
+                    Cases.update(caseId,{$set:{supportPersonId:team.level1[0]}});
+                    expect(()=>{
+                        assign_case_to_another_team.apply({
+                            userId: team.level1[1],
+                            user: Meteor.users.findOne({_id:team.level1[1] }),
+                        },[{caseId,teamName:'Developer'}]);
+                    }).to.throw();
+                });
+
+                it('can\'t assign an not exists case',()=>{
+                    expect(()=>{
+                        assign_case_to_another_team.apply({
+                            userId: team.level1[0],
+                            user: Meteor.users.findOne({_id:team.level1[0] }),
+                        },[{caseId:Random.id(), teamName:'Developer'}]);
+                    }).to.throw();
+                });
+
+                it('can\'t assign to an not exists team',()=>{
+                    expect(()=>{
+                        assign_case_to_another_team.apply({
+                            userId: team.level1[0],
+                            user: Meteor.users.findOne({_id:team.level1[0] }),
+                        },[{caseId,teamName:Random.id()}]);
+                    }).to.throw();
+                });
+
+                it('can assign to itself', ()=>{
+                    const supportPersonId = team.level1[0];
+                    assign_case_to_self.apply({
+                        userId: supportPersonId,
+                        user: Meteor.users.findOne({_id:supportPersonId}),
+                    },[{caseId}]);
+                    const the_case = Cases.findOne({_id:caseId});
+                    assert.equal(the_case.supportPersonId, supportPersonId);
+                    assert(the_case.state, State.PROCESSING);
+                });
+
+                it('can release the case back to the team', ()=>{
+                    const supportPersonId = team.level1[0];
+                    Cases.update(caseId, { $set: {
+                        supportPersonId:supportPersonId,
+                        state:State.PROCESSING,
+                    }});
+                    release.apply({
+                        userId: supportPersonId,
+                        user: Meteor.users.findOne({_id:supportPersonId }),
+                    },[{caseId}]);
+                });
+
+            });// end assign cases
+            describe('Note',()=>{
+                const add_note = Meteor.server.method_handlers[
+                    MethodNames.note.add
+                ];
+                it('can add a note',()=>{
+                    CaseNotes.remove({});
+                    const supportPerson = Meteor.users
+                              .findOne({_id:team.level1[0]});
+                    Cases.update(caseId,{$set:{
+                        supportTeamName: Configs.DEFAULT_SUPPORT_TEAM,
+                        supportPersonId: supportPerson._id,
+                    }});
+                    const content = Random.id();
+                    add_note.apply({
+                        userId: supportPerson._id,
+                        user: supportPerson
+                    },[{caseId,content}]);
+                    const the_note = CaseNotes.findOne({});
+                    assert.equal(the_note.caseId,caseId);
+                    assert.equal(the_note.supportPersonId,supportPerson._id);
+                    assert.equal(the_note.content, content);
+                });
             });
+        });//end method
 
-            it('can\'t assign an not exists case',()=>{
-                expect(()=>{
-                    assign_case_to_another_team.apply({
-                        userId: team.level1[0],
-                        user: Meteor.users.findOne({_id:team.level1[0] }),
-                    },[{caseId:Random.id(), teamName:'Developer'}]);
-                }).to.throw();
-            });
-
-            it('can\'t assign to an not exists team',()=>{
-                expect(()=>{
-                    assign_case_to_another_team.apply({
-                        userId: team.level1[0],
-                        user: Meteor.users.findOne({_id:team.level1[0] }),
-                    },[{caseId,teamName:Random.id()}]);
-                }).to.throw();
-            });
-
-            it('can assign to itself', ()=>{
-                const supportPersonId = team.level1[0];
-                assign_case_to_self.apply({
-                    userId: supportPersonId,
-                    user: Meteor.users.findOne({_id:supportPersonId}),
-                },[{caseId}]);
-                const the_case = Cases.findOne({_id:caseId});
-                assert(the_case.supportPersonId, supportPersonId);
-                assert(the_case.state, State.PROCESSING);
-            });
-
-            it('can release the case back to the team', ()=>{
-                const supportPersonId = team.level1[0];
-                Cases.update(caseId, { $set: {
-                    supportPersonId:supportPersonId,
-                    state:State.PROCESSING,
-                }});
-                release.apply({
-                    userId: supportPersonId,
-                    user: Meteor.users.findOne({_id:supportPersonId }),
-                },[{caseId}]);
-            });
-
-        });
-        /*
-        expect(function () {
-            Meteor.call('itemCreate', item);
-        }).toThrow()*/
-
-    });
-    /*
-    describe('Customer', () => {
-        describe('methods',()=>{
-            const customerId = Random.id();
-            beforeEach(() => {
-                Cases.remove({});
-            });
-
-            it('can open a case', (done) => {
-                const new_case = Meteor.server.method_handlers[MethodNames.case.add];
-                const content = "test content";
-                new_case.apply({
-                    userId: customerId
-                },[{content}]);
-                the_case = Cases.find({customerId: customerId});
-                assert.equal(the_case.count(), 1);
-                the_case = the_case.fetch()[0];
-                assert.equal(the_case.state, State.PENDING, "default state should be PENDING");
-                assert.equal(the_case.customerId, customerId);
-                assert.equal(the_case.content, content);
-                done();
-            });
-
-        });
-        describe('publications', () => {
-
-            Factory.define('case', Cases, {});
-            const new_case = ({customerId=Random.id(),createdAt=new Date(), content="A new content", state=State.PENDING}) =>{
-                Factory.create('case',{customerId, createdAt, content, state});
-            };
-            const currentCustomer = Random.id();
-            before(()=>{
-                Cases.remove({});
-                _.times(3, new_case);
-                _.times(3, () => new_case({customerId:currentCustomer}));
-            });
-
-            it('can only view own cases', (done)=>{
-                const collector = new PublicationCollector({ userId: currentCustomer});
-                collector.collect(Publications.cases.list,(collections) => {
-                    assert.equal(collections.Cases.length, 3);
-                 });
-
-                done();
-            });
-        });
-    });
-     */
+    });// end support person
 }
